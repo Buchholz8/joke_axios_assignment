26981ea16ada87d431ce3370917f74d5ae68f75a
markb <markbuchholz8@gmail.com>
2023-04-21 11:57:32 -0600

    install axios

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+<normal-joke-button></normal-joke-button>
+  </div>
+</template>
+
+<script>
+import NormalJokeButton from '@/components/NormalJokeButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+NormalJokeButton,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NormalJokeButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button>Normal Joke</button>
+    <div v-for="(joke, i) in jokes" :key="i">
+      <p>{{ joke }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      jokes: undefined,
+    };
+  },
+  mounted() {
+    axios.request({
+        url: `https://ron-swanson-quotes.herokuapp.com/v2/quotes`,
+      })
+      .then((response) => {
+        this.joke = response[`data`][`data`];
+      })
+      .catch((error) => {
+        error;
+      });
+  },
+};
+</script>
+
+<style scoped>
+</style>
